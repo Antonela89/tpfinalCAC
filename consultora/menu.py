2991aa2ca1d61524ed70db0224b0735de1f8d075
Antonela Borgogno <antonelaborgogno@gmail.com>
2022-11-09 14:00:52 -0300

    agregar trabajador y reportes trabajador

--- consultora/menu.py
+++ consultora/menu.py
@@ -1,13 +1,3 @@
-#apertura de archivo:
-#trabajadores=[]
-#with open("trabajadores.dat", "a") as trabajadores:
-# t=trabajador.replace("\n","")
-# t=t.split(",")
-# trabajadores.append(t)
-
-# trabajadores.close()
-
-
 while True:
     print(f'''
         Menu:
@@ -34,25 +24,34 @@ while True:
         if (opcion == 0):
             break
         elif (opcion == 1):
+
             print('Ingresar nuevo Trabajador')
-            # while True:
-                # nombre=input("Nombre('x' para salir): ")
-                # if nombre == 'x':
-                #     break
-                # edad = int(input("Edad: "))
-                # profesion = input("Porofesion: ")
-                # activo = input("Esta trabajando? (s/n): ")
-                # if activo == "s" or activo =="S": 
-                #     activo = True
-                # else:
-                #     activo = False
-                # trabajador = (nombre, edad, profesion, activo)
-                # trabajadores.append(trabajador)
+            while True:
+                nombre=input("Nombre('x' para salir): ")
+                if nombre == 'x':
+                    break
+                edad = input("Edad: ")
+                dni = input("Dni: ")
+                profesion = input("Profesion: ")
+                activo = input("Esta trabajando? (s/n): ")
+                if activo == "s" or activo =="S": 
+                    activo = True
+                else:
+                    activo = False
+                
+                trabajadores = open("trabajadores.dat", "a")
+                trabajadores.write(f'''{nombre},{edad},{dni},{profesion},{activo}\n''')
+
+                trabajadores.close()
+
         elif (opcion == 2):
             print('Modificar dato de trabajador (file.writelines())')
-            #acciones para modificar trabajador
-                #encontrar un trabajador
-                #modificarlo
+            # dato = input("Ingrese dato: ")
+            # nuevoDato = input("Ingrese nuevo dato: ")
+            # modificar = open("trabajadores.dat", "r+")
+            # modificar.writelines([nuevoDato])
+
+            trabajadores.close()
         elif (opcion == 3):
             print('Eliminar Trabajador')
             #acciones para eliminar trabajador
@@ -74,31 +73,69 @@ while True:
             break
         elif (opcion == 1):
             print('Mostrar trabajadores Activos')
-            # for trabajador in trabajadores:
-            #     if trabajador[5] == True:
-            #         print(trabajador)
+            trabajadores = open("trabajadores.dat", "r")
+            listado=[]
+            for renglon in trabajadores.readlines():
+                var=renglon.split(",")
+                trabajador={"nombre":var[0], "edad":int(var[1]), "dni":int(var[2]), "profesion":var[3], "activo":(var[4].replace("\n",""))}
+                
+                estado=trabajador['activo']
+                if estado == 'True':
+                    listado.append(trabajador)
+            print(listado)
+
+            trabajadores.close()
         elif (opcion == 2):
             print('Mostrar trabajadores desocupados')
-            # for trabajador in trabajadores:
-            #     if trabajador[5] == False:
-            #         print(trabajador)
+            trabajadores = open("trabajadores.dat", "r")
+            listado=[]
+            for renglon in trabajadores.readlines():
+                var=renglon.split(",")
+                trabajador={"nombre":var[0], "edad":int(var[1]), "dni":int(var[2]), "profesion":var[3], "activo":(var[4].replace("\n",""))}
+                
+                estado=trabajador['activo']
+                if estado == 'False':
+                    listado.append(trabajador)
+            print(listado)
+
+            trabajadores.close()
         elif (opcion == 3):
             print('Mostrar desocupados en un rango de edad')
-            # desde = int(input("Ingrese rango de inicio: "))
-            # hasta = int(input("Ingrese rango de fin: "))
-            # for trabajor in trabajadores:
-            #     if ((trabajor[5] == False) in range(desde, hasta + 1)):
-            #         print(trabajor)
+            desde = input(int("Ingrese edade desde: "))
+            hasta = input(int("Ingrese edade hasta: "))
+            
+            trabajadores = open("trabajadores.dat", "r")
+            listado=[]
+            for renglon in trabajadores.readlines():
+                var=renglon.split(",")
+                trabajador={"nombre":var[0], "edad":int(var[1]), "dni":int(var[2]), "profesion":var[3], "activo":(var[4].replace("\n",""))}
+                
+                estado=trabajador['activo']
+                if estado == 'False' and range(desde, hasta+1):
+                    listado.append(trabajador)
+            print(listado)
+
+            trabajadores.close()
         elif (opcion == 4):
             print('Mostrar trabajadores segun la profesion')
-            # profesion = input("Elegir profesion: ")
-            # profesionEncontrada = False
-            # for trabajador in trabajadores:
-            #     if trabajador[4].lower() == profesion.lower():
-            #         print(trabajador)
-            # profesionEncontrada = True
-            # if not profesionEncontrada:
-            #     print("Profesión no existente")   
+            profesion = input("Elegir profesion: ")
+
+            trabajadores = open("trabajadores.dat", "r")
+
+            listado=[]
+            profesionEncontrada = False
+
+            for renglon in trabajadores.readlines():
+                var=renglon.split(",")
+                trabajador={"nombre":var[0], "edad":int(var[1]), "dni":int(var[2]), "profesion":var[3], "activo":(var[4].replace("\n",""))}
+                
+                estado = trabajador['profesion']
+                if estado.lower() == profesion.lower():
+                    listado.append(trabajador)
+                    profesionEncontrada = True
+                if not profesionEncontrada:
+                    print("Profesión no existente")
+            print(listado)
 
     elif (opcion == 3):
         print('Cambiar status trabajador: ')
