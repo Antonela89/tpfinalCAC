826a312fe40eaa38df65126b7537b08f3703286d
Antonela Borgogno <antonelaborgogno@gmail.com>
2022-11-12 00:27:48 -0300

    modificar trabajador

--- consultora/crud.py
+++ consultora/crud.py
@@ -1,12 +1,14 @@
 import json
 # funciones de gestion de datos:
 
-#abrir un archivo
-def abrirArchivo(archivo,modo):
+# abrir un archivo
+
+
+def abrirArchivo(archivo, modo):
     trabajadores = open(archivo, modo)
     return trabajadores
 
-#crear trabajador:
+# crear trabajador:
 def agregarTrabajador():
     while True:
         nombre = input("Nombre('x' para salir): ")
@@ -20,30 +22,37 @@ def agregarTrabajador():
             activo = True
         else:
             activo = False
-        trabajadores = open("trabajadores.dat", "a")
+        trabajadores = open("trabajadores.dat", "w")
         trabajadores.write(f'''{nombre},{edad},{dni},{profesion},{activo}\n''')
         trabajadores.close()
     return print(">>>Trabajador ingresado")
 
-#modificar datos de un trabajador
+# modificar datos de un trabajador
 
-#eliminar trabajador de archivo
+# eliminar trabajador de archivo
 
-#imprimir listado de base de datos:
+# imprimir listado de base de datos:
+
+
 def imprimirLista(lista):
     print(json.dumps(lista, sort_keys=False, indent=4))
 
-#Recorrer base de datos:
-def armarReporte(key,value):
-    listado=[]
+# Recorrer base de datos:
+
+
+def armarReporte(key, value):
+    listado = []
     trabajadores = open("trabajadores.dat", "r")
     for renglon in trabajadores.readlines():
+        # devuelve lista ["Ingrid","14","47845231", "Estudiante", "False\n"]
         var = renglon.split(",")
-        trabajador = {"Nombre": var[0], "Edad": int(var[1]), "Dni": int(
-        var[2]), "Profesion": var[3], "Activo": (var[4].replace("\n", ""))}
+        trabajador = {"Nombre": var[0], "Edad": int(var[1]), "Dni": int(var[2]), "Profesion": var[3], "Activo": (
+            var[4].rstrip())}  # o replace("\n", "") -rstrip: metodo string: elimina caracter fantasmas (\n, \t...)
         trabajadores.close()
         estado = trabajador[key]
         if estado == value:
             listado.append(trabajador)
     return imprimirLista(listado)
+
+#replace("\n", "")
 
--- consultora/menu.py
+++ consultora/menu.py
@@ -35,47 +35,57 @@ while True:
             trabajadores = open("trabajadores.dat", "r")
             listado = []
             for renglon in trabajadores.readlines():
-                var = renglon.split(",")
-                trabajador = {"Dni": int(var[2]), "Nombre": var[0], "Edad": int(var[1]),  "Profesion": var[3], "Activo": (var[4].replace("\n", ""))}
-                listado.append(trabajador)
-            crud.imprimirLista(listado)
+                listado.append((renglon).replace("\n",""))
+            print(listado)
+            trabajadores.close()
 
             referencia = input("Ingrese dni: ")
+            for elemento in listado:
+                if referencia in elemento:
+                    elemento=elemento.split(",")
+                    print(elemento)
 
-            dni = trabajador['Dni']
-            print(renglon)
+                    while True:
+                        print(f'''
+                            Que desea modificar:
+                            [1] Nombre
+                            [2] Edad
+                            [3] Dni
+                            [4] Profesion
+                            [5] Activo
+                            [0] Salir
+                        ''')
 
-            while True:
-                print(f'''
-                    Que desea modificar:
-                    [1] Dni
-                    [2] Nombre
-                    [3] Edad
-                    [4] Profesion
-                    [5] Activo
-                    [0] Salir
-                ''')
+                        opcion = excepcion.correccionErrores("Ingrese una opcion: ")
+                        if (opcion == 0):
+                            break
+                        elif (opcion == 1):
+                            nuevoDato = input("Ingrese nuevo nombre: ")
+                            elemento[0] = nuevoDato
+                            print(elemento)
 
-                opcion = excepcion.correccionErrores("Ingrese una opcion: ")
-                if (opcion == 0):
-                    break
-                elif (opcion == 1):
-                    nuevoDato = input("Ingrese nuevo Dni: ")
-                    for i in var:
-                        var[2] = nuevoDato
-                    cambio = ",".join([str(elem) for elem in var])
-                    print(cambio)
-                    trabajadores.writelines(cambio)
-                elif (opcion == 2):
-                    nuevoDato = input("Ingrese nuevo Nombre: ")
-                elif (opcion == 3):
-                    nuevoDato = input("Ingrese nueva Edad: ")
-                elif (opcion == 4):
-                    nuevoDato = input("Ingrese nueva Profesión: ")
-                elif (opcion == 5):
-                    nuevoDato = input("Ingrese nuevo estado: ")
 
-            trabajadores.close()
+                            a = open("trabajadores.dat","w")
+                            a.writelines(f'''{elemento[0]},{elemento[1]},{elemento[2]},{elemento[3]},{elemento[4]}\n''')
+                            a.close()
+                        elif (opcion == 2):
+                            nuevoDato = input("Ingrese nueva edad: ")
+                            elemento[1] = nuevoDato
+                            print(elemento)
+                        elif (opcion == 3):
+                            nuevoDato = input("Ingrese nuevo dni: ")
+                            elemento[2] = nuevoDato
+                            print(elemento)
+                        elif (opcion == 4):
+                            nuevoDato = input("Ingrese nueva profesión: ")
+                            elemento[3] = nuevoDato
+                            print(elemento)
+                        elif (opcion == 5):
+                            nuevoDato = input("Ingrese nuevo estado: ")
+                            elemento[4] = nuevoDato
+                            print(elemento)
+
+
         elif (opcion == 3):
             print('Eliminar Trabajador')
             # acciones para eliminar trabajador
@@ -83,66 +93,67 @@ while True:
             # eliminarlo
 
     elif (opcion == 2):
-        print(f'''
-            Reportes:
-            [1] Mostrar trabajadores Activos
-            [2] Mostrar trabajadores desocupados
-            [3] Mostrar desocupados en un rango de edad
-            [4] Mostrar trabajadores segun la profesion
-            [0] Salir
-        ''')
-
-        opcion = int(input("Ingrese una opcion: "))
-        if (opcion == 0):
-            break
-        elif (opcion == 1):
-            print('Mostrar trabajadores Activos')
-            crud.armarReporte('Activo', 'True')
-
-        elif (opcion == 2):
-            print('Mostrar trabajadores desocupados')
-            crud.armarReporte('Activo', 'False')
-
-        elif (opcion == 3):
-            print('Mostrar desocupados en un rango de edad')
-            desde = int(input("Ingrese edad desde: "))
-            hasta = int(input("Ingrese edad hasta: "))
-            
-            listado = []
-            trabajadores = open("trabajadores.dat", "r")
-            for renglon in trabajadores.readlines():
-                var = renglon.split(",")
-                trabajador = {"nombre": var[0], "edad": int(var[1]), "dni": int(
-                    var[2]), "profesion": var[3], "activo": (var[4].replace("\n", ""))}
-                estado = trabajador['activo']
-                edad = trabajador['edad']
-                if estado == 'False' and edad in range(desde, hasta+1):
-                    listado.append(trabajador)
-            crud.imprimirLista(listado)
-
-            trabajadores.close()
-        elif (opcion == 4):
-            print('Mostrar trabajadores segun la profesion')
-            profesion = input("Elegir profesion: ")
-
-            trabajadores = open("trabajadores.dat", "r")
-            listado = []
-            profesionEncontrada = False
-            for renglon in trabajadores.readlines():
-                var = renglon.split(",")
-                trabajador = {"nombre": var[0], "edad": int(var[1]), "dni": int(
-                    var[2]), "profesion": var[3], "activo": (var[4].replace("\n", ""))}
-
-                estado = trabajador['profesion']
-                if estado.lower() == profesion.lower():
-                    listado.append(trabajador)
-                    profesionEncontrada = True
-            if not profesionEncontrada:
-                print("Profesión no existente")
-            crud.imprimirLista(listado)
-        else:
-            print('Ingresa una opcion valida')
-
+        while True:
+            print(f'''
+                Reportes:
+                [1] Mostrar trabajadores Activos
+                [2] Mostrar trabajadores desocupados
+                [3] Mostrar desocupados en un rango de edad
+                [4] Mostrar trabajadores segun la profesion
+                [0] Salir
+            ''')
+    
+            opcion = int(input("Ingrese una opcion: "))
+            if (opcion == 0):
+                break
+            elif (opcion == 1):
+                print('Mostrar trabajadores Activos')
+                crud.armarReporte('Activo', 'True')
+    
+            elif (opcion == 2):
+                print('Mostrar trabajadores desocupados')
+                crud.armarReporte('Activo', 'False')
+    
+            elif (opcion == 3):
+                print('Mostrar desocupados en un rango de edad')
+                desde = int(input("Ingrese edad desde: "))
+                hasta = int(input("Ingrese edad hasta: "))
+                
+                listado = []
+                trabajadores = open("trabajadores.dat", "r")
+                for renglon in trabajadores.readlines():
+                    var = renglon.split(",")
+                    trabajador = {"nombre": var[0], "edad": int(var[1]), "dni": int(
+                        var[2]), "profesion": var[3], "activo": (var[4].replace("\n", ""))}
+                    estado = trabajador['activo']
+                    edad = trabajador['edad']
+                    if estado == 'False' and edad in range(desde, hasta+1):
+                        listado.append(trabajador)
+                crud.imprimirLista(listado)
+    
+                trabajadores.close()
+            elif (opcion == 4):
+                print('Mostrar trabajadores segun la profesion')
+                profesion = input("Elegir profesion: ")
+    
+                trabajadores = open("trabajadores.dat", "r")
+                listado = []
+                profesionEncontrada = False
+                for renglon in trabajadores.readlines():
+                    var = renglon.split(",")
+                    trabajador = {"nombre": var[0], "edad": int(var[1]), "dni": int(
+                        var[2]), "profesion": var[3], "activo": (var[4].replace("\n", ""))}
+    
+                    estado = trabajador['profesion']
+                    if estado.lower() == profesion.lower():
+                        listado.append(trabajador)
+                        profesionEncontrada = True
+                if not profesionEncontrada:
+                    print("Profesión no existente")
+                crud.imprimirLista(listado)
+            else:
+                print('Ingresa una opcion valida')
+    
     elif (opcion == 3):
         print('Cambiar status trabajador: ')
     else:
